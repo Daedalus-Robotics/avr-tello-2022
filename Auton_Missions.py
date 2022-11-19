174743466dee254f8c16cb36cdf9466d59358acc
Arya Lohia <96156566+a-lohia@users.noreply.github.com>
2022-11-19 12:54:42 -0500

    V2

--- Auton_Missions.py
+++ Auton_Missions.py
@@ -1,20 +1,26 @@
 from djitellopy import Tello
 
 
-FIRESTATION_TO_FIRSTBUILDING_LATERAL = 0
+FIRESTATION_TO_FIRSTBUILDING_LATERAL = 35
+
+#Test values no y heights
+
+TEXTRON_TALL_Y = 20
+TEXTRON_MIDDLE_Y = 20
+TEXTRON_SMALL_Y = 20
 
 #TEXTRON TOWERS y Values CM
 
-TEXTRON_TALL_Y = 0 
-TEXTRON_MIDDLE_Y = 0
-TEXTRON_SMALL_Y = 0
+#TEXTRON_TALL_Y = 200 
+#TEXTRON_MIDDLE_Y = 80
+#TEXTRON_SMALL_Y = 20
 
 
 #Residential District y Values CM
 
-RESIDENTIAL_TALL_Y = 0
-RESIDENTIAL_MIDDLE_Y = 0 
-RESIDENTIAL_SMALL_Y = 0
+RESIDENTIAL_TALL_Y = 165
+RESIDENTIAL_MIDDLE_Y = 127
+RESIDENTIAL_SMALL_Y = 20
 
 
 
@@ -39,7 +45,7 @@ def triangulate_small(t: Tello) -> tuple:
     z_dist = t.get_mission_pad_distance_z()
 
     id = t.get_mission_pad_id()
-    t.go_xyz_speed_mid(0, 0, z_dist, 30, mid=id)
+    t.go_xyz_speed_mid(x=1, y=1, z=z_dist, speed=30, mid=id)
 
 def triangulate_tall(t: Tello) -> tuple:
     x_dist = t.get_mission_pad_distance_x()
@@ -47,7 +53,7 @@ def triangulate_tall(t: Tello) -> tuple:
     z_dist = t.get_mission_pad_distance_z()
 
     id = t.get_mission_pad_id()
-    t.go_xyz_speed_mid(0, 0, z_dist, 30, mid=id)
+    t.go_xyz_speed_mid(z=z_dist, speed=30, mid=id)
 
 def loop_forward(dist: int, mp: int) -> None:
     while(tello.get_mission_pad_id() != mp):
@@ -55,10 +61,12 @@ def loop_forward(dist: int, mp: int) -> None:
     
 
 #pass residential if the side is residential
-is_textron = side()
+burn = str(input("which side textron, or residential: "))
+is_textron = side(burn)
 
 #1 -3
-mission_type = mission()
+miss = int(input("mission type, 1, 2, or 3: "))
+mission_type = 1
 
 # Mission
 tello.takeoff()
@@ -81,7 +89,7 @@ if(is_textron):
     triangulate_small(tello)
     triangulate_small(tello)
 
-    tello.move_up(TEXTRON_MIDDLE_Y) #Above first pad
+    tello.move_up(TEXTRON_MIDDLE_Y-TEXTRON_SMALL_Y) #Above first pad
     
 
 
@@ -135,5 +143,69 @@ if(is_textron):
 
 else:
     #residential
-    tello.end()
+    tello.move_up(RESIDENTIAL_SMALL_Y)
+    
+    #orient to direction of first building
+    if(mission_type==1):
+        tello.move_left(FIRESTATION_TO_FIRSTBUILDING_LATERAL)
+    else:
+        tello.move_right(FIRESTATION_TO_FIRSTBUILDING_LATERAL)
+
+
+    loop_forward(40, 1) if mission_type == 1 else loop_forward(40, 2)
+    
+    
+    triangulate_small(tello)
+    triangulate_small(tello)
+
+    tello.move_up(RESIDENTIAL_MIDDLE_Y) #Above first pad
+    
+
+
+    if(mission_type==1):
+        tello.rotate_clockwise(45)
+        loop_forward(40, 4)
+        tello.rotate_counter_clockwise(45) #Above second pad
+        
+    else:
+        tello.move_forward(40,4)
+
+    triangulate_small(tello)
+    triangulate_small(tello)
+    #triangulated to MP4
+
+    
+
+    if(mission_type==1 or mission_type==3):
+        tello.move_up(RESIDENTIAL_TALL_Y) #move to height of tall buildings
+
+        loop_forward(40, 6)
+        triangulate_tall(tello)
+        triangulate_tall(tello)
+        #above MP6
+
+        tello.rotate_counter_clockwise(90) #facing MP5
+
+        loop_forward(40, 5)
+        triangulate_small(tello)
+        triangulate_small(tello)
+        tello.land()
+
+
+    else:
+        tello.rotate_counter_clockwise(90)
+        loop_forward(40,3)
+        triangulate_small(tello)
+        tello.rotate_clockwise(90)
+        triangulate_small(tello)
+        #above MP3, facing MP5
+
+        tello.move_up(RESIDENTIAL_TALL_Y) #move to height of tall buildings
+
+        loop_forward(40, 5)
+        triangulate_small(tello)
+        triangulate_small(tello)
+        tello.land()
+
+tello.end()
 
