eec8c98df1ee258e1414b1339ff29bd5501f6496
Arya Lohia <96156566+a-lohia@users.noreply.github.com>
2022-11-25 11:15:15 -0500

    newest

--- resources/ControllerInput.py
+++ resources/ControllerInput.py
@@ -2,6 +2,7 @@ from inputs import get_gamepad
 import math
 import threading
 
+
 class XboxController(object):
     MAX_TRIG_VAL = math.pow(2, 8)
     MAX_JOY_VAL = math.pow(2, 15)
@@ -28,39 +29,36 @@ class XboxController(object):
         self.RightDPad = 0
         self.UpDPad = 0
         self.DownDPad = 0
-        
 
         self._monitor_thread = threading.Thread(target=self._monitor_controller, args=())
         self._monitor_thread.daemon = True
         self._monitor_thread.start()
 
-
-    def read(self): # return the buttons/triggers that you care about in this method
+    def read(self):  # return the buttons/triggers that you care about in this method
         x = self.LeftJoystickX
         y = self.LeftJoystickY
         a = self.A
-        b = self.X # b=1, x=2
+        b = self.X  # b=1, x=2
         rb = self.RightBumper
         return [x, y, a, b, rb]
-
 
     def _monitor_controller(self):
         while True:
             events = get_gamepad()
-    
+
             for event in events:
                 if event.code == 'ABS_Y':
-                    self.LeftJoystickY = event.state / XboxController.MAX_JOY_VAL # normalize between -1 and 1
+                    self.LeftJoystickY = event.state / XboxController.MAX_JOY_VAL  # normalize between -1 and 1
                 elif event.code == 'ABS_X':
-                    self.LeftJoystickX = event.state / XboxController.MAX_JOY_VAL # normalize between -1 and 1
+                    self.LeftJoystickX = event.state / XboxController.MAX_JOY_VAL  # normalize between -1 and 1
                 elif event.code == 'ABS_RY':
-                    self.RightJoystickY = event.state / XboxController.MAX_JOY_VAL # normalize between -1 and 1
+                    self.RightJoystickY = event.state / XboxController.MAX_JOY_VAL  # normalize between -1 and 1
                 elif event.code == 'ABS_RX':
-                    self.RightJoystickX = event.state / XboxController.MAX_JOY_VAL # normalize between -1 and 1
+                    self.RightJoystickX = event.state / XboxController.MAX_JOY_VAL  # normalize between -1 and 1
                 elif event.code == 'ABS_Z':
-                    self.LeftTrigger = event.state / XboxController.MAX_TRIG_VAL # normalize between 0 and 1
+                    self.LeftTrigger = event.state / XboxController.MAX_TRIG_VAL  # normalize between 0 and 1
                 elif event.code == 'ABS_RZ':
-                    self.RightTrigger = event.state / XboxController.MAX_TRIG_VAL # normalize between 0 and 1
+                    self.RightTrigger = event.state / XboxController.MAX_TRIG_VAL  # normalize between 0 and 1
                 elif event.code == 'BTN_TL':
                     self.LeftBumper = event.state
                 elif event.code == 'BTN_TR':
@@ -68,9 +66,9 @@ class XboxController(object):
                 elif event.code == 'BTN_SOUTH':
                     self.A = event.state
                 elif event.code == 'BTN_NORTH':
-                    self.Y = event.state #previously switched with X
+                    self.Y = event.state  # previously switched with X
                 elif event.code == 'BTN_WEST':
-                    self.X = event.state #previously switched with Y
+                    self.X = event.state  # previously switched with Y
                 elif event.code == 'BTN_EAST':
                     self.B = event.state
                 elif event.code == 'BTN_THUMBL':
@@ -82,12 +80,8 @@ class XboxController(object):
                 elif event.code == 'BTN_START':
                     self.Start = event.state
                 elif event.code == 'ABS_HAT0X':
-                    self.LeftDPad = 1 if event.state==-1 else 0 
-                    self.RightDPad = 1 if event.state==1 else 0 
+                    self.LeftDPad = 1 if event.state == -1 else 0
+                    self.RightDPad = 1 if event.state == 1 else 0
                 elif event.code == 'ABS_HAT0Y':
                     self.UpDPad = 1 if event.state == -1 else 0
-                    self.DownDPad = 1 if event.state ==1 else 0
-    
-                #else:
-                    #print(f"code: {event.code}. value: {event.state}")
-
+                    self.DownDPad = 1 if event.state == 1 else 0
